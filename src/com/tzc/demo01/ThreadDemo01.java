2faf5fd2dd4f7bd239d1ca7660e3b9fb4ea06ad3
XuHao <XuHao@tzc.com>
2022-03-04 11:47:54 +0800

    hot-fix first

--- src/com/tzc/demo01/ThreadDemo01.java
+++ src/com/tzc/demo01/ThreadDemo01.java
@@ -7,11 +7,12 @@ package com.tzc.demo01;
 public class ThreadDemo01 {
     public static void main(String[] args) {
         //4.在测试类中,需要多少条线程就创建多少个Thread类的子类对象
-        SubThread s1 = new SubThread("自定义线程1");
-        SubThread s2 = new SubThread("自定义线程2");
+        SubThread s1 = new SubThread("自定义线程11");
+        SubThread s2 = new SubThread("自定义线程22");
+        SubThread s3 = new SubThread("自定义线程33");
 
         //5.开启线程
-        s1.start();
         s2.start();
+        s1.start();
     }
 }
